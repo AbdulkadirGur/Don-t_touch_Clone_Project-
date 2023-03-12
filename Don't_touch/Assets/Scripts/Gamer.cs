1b6d8ee42dfb7317cdaf67be3a122b7bddbc1455
Abdulkadir Gür <58683068+AbdulkadirGur@users.noreply.github.com>
2023-03-13 02:25:08 +0300

    Add new Powerbar

--- Don't_touch/Assets/Scripts/Gamer.cs
+++ Don't_touch/Assets/Scripts/Gamer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gamer : MonoBehaviour
 {
@@ -8,29 +9,72 @@ public class Gamer : MonoBehaviour
     public GameObject bombSpawn;
     public ParticleSystem bombEffect;
     public AudioSource bombSound;
-    
+
+    public Image PowerBar;
+    float powerNum;
+
+    bool finishflag=false;
+
+    Coroutine powerLoop;
+
     void Start()
     {
-        
+        powerLoop = StartCoroutine(PowerBarWork());
     }
 
-    
+    IEnumerator PowerBarWork()
+    {
+        finishflag = false;
+        PowerBar.fillAmount = 0;
+
+        while (true )
+        {
+            if (PowerBar.fillAmount < 1 && !finishflag)
+            {
+                powerNum = 0.01f;
+                PowerBar.fillAmount += powerNum;
+                yield return new WaitForSeconds(0.1f * Time.deltaTime);
+            }
+            else
+            {
+                powerNum = 0.01f;
+                finishflag = true;
+                PowerBar.fillAmount -= powerNum;
+                yield return new WaitForSeconds(0.1f * Time.deltaTime);
+
+                if (PowerBar.fillAmount==0)
+                {
+                    finishflag = false;  
+                }
+               
+            }         
+            
+        }
+    }
     void Update()
     {
 
         if(Input.GetKeyDown(KeyCode.Space)) 
         {
+            
             Instantiate(bombEffect, bombSpawn.transform.position, bombSpawn.transform.rotation);
             bombSound.Play();
             GameObject bombs = Instantiate(bomb ,bombSpawn.transform.position, bombSpawn.transform.rotation);// create the bombs
             Rigidbody2D rg = bombs.GetComponent<Rigidbody2D>();
-            rg.AddForce(new Vector2(2f, 0f) * 8f, ForceMode2D.Impulse); // apply force to bombs
+            rg.AddForce(new Vector2(2f, 0f) * PowerBar.fillAmount * 12f, ForceMode2D.Impulse); // apply force to bombs
+
+            StopCoroutine(powerLoop);
+            
          
 
-
-
-
         }
-        
+       
+
+    }
+
+    public void PowerPlay()
+    {
+
+        powerLoop = StartCoroutine(PowerBarWork());
     }
 }
--- Don't_touch/Assets/Scripts/bombScript.cs
+++ Don't_touch/Assets/Scripts/bombScript.cs
@@ -8,41 +8,52 @@ public class bombScript : MonoBehaviour
     
 
     GameObject gamemanager;
+    GameObject Player;
     private void Start()
     {
         power = 20;
         gamemanager = GameObject.FindWithTag("GameManagerTag");
+        Player = GameObject.FindWithTag("player1");
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("midboxTag"))
         {
-            collision.gameObject.GetComponent<midboxScript>().Damage(power); // Damage the boxes  .
 
+            collision.gameObject.GetComponent<midboxScript>().Damage(power); // Damage the boxes  .
+            Player.GetComponent<Gamer>().PowerPlay();//  PlayPowerbar again.
             gamemanager.GetComponent<gameManager>().Create_Sound_and_Effect(0,collision.gameObject);// Effect
 
 
             Destroy(gameObject);
             //GetComponent<CircleCollider2D>().isTrigger = false; // 
         }
-        if (collision.gameObject.CompareTag("Gamer2Tag"))
+        if (collision.gameObject.CompareTag("Gamer2Tag") || collision.gameObject.CompareTag("player2"))
         {
             gamemanager.GetComponent<gameManager>().Damage_Manager(0,power); // Damage the Gamer2  .
-
+            Player.GetComponent<Gamer>().PowerPlay();//  PlayPowerbar again.
             gamemanager.GetComponent<gameManager>().Create_Sound_and_Effect(0, collision.gameObject);// Effect
 
             Destroy(gameObject);
-            //GetComponent<CircleCollider2D>().isTrigger = false; // 
+            
         }
-        if (collision.gameObject.CompareTag("Gamer1Tag"))
+        if (collision.gameObject.CompareTag("Gamer1Tag") || collision.gameObject.CompareTag("player1"))
         {
             gamemanager.GetComponent<gameManager>().Damage_Manager(1, power);// Damage the Gamer1  .
-
+            Player.GetComponent<Gamer>().PowerPlay();//  PlayPowerbar again.
             gamemanager.GetComponent<gameManager>().Create_Sound_and_Effect(0, collision.gameObject); // Effect
 
             Destroy(gameObject);
-            //GetComponent<CircleCollider2D>().isTrigger = false; // 
+            
+        }
+
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            gamemanager.GetComponent<gameManager>().Create_Sound_and_Effect(0, collision.gameObject); // Effect
+            Player.GetComponent<Gamer>().PowerPlay();//  PlayPowerbar again.
+            Destroy(gameObject);
+
         }
     }
 }
